fb4799bc4434f17e71355e522d02fec50bb3eb36
Andrea Futuri <ceo@andreafuturi.com>
2024-12-03 12:20:01 +0200

    WIP-03-12-2024-12-20-01}

--- client/about/about.jsx
+++ client/about/about.jsx
@@ -3,7 +3,7 @@ import { inlineImport } from "../../lib/framework-utils.jsx";
 export default function About() {
   return (
     <about>
-      {/* {inlineImport({ src: "./about.css" })} */}
+      {inlineImport({ src: "./about.css" })}
       About
     </about>
   );
