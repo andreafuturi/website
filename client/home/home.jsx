11212cdd71661820841ef821beb27d250da431e2
Andrea Futuri <ceo@andreafuturi.com>
2024-12-04 10:50:21 +0200

    WIP-04-12-2024-10-50-21}

--- client/home/home.jsx
+++ client/home/home.jsx
@@ -34,13 +34,9 @@ function Home() {
         <small class="center">Trusted by +10 companies</small>
         <logos>{[carrefourLogo, recrowdLogo, gustirariLogo, worldyLogo]}</logos>
       </companies>
-      <a class="cta">Book a call now</a>
-      <a class="center scroll-to-about flex-col" href="/about">
-        About
-        <svg fill="currentColor" width="17" height="11" viewBox="0 0 17 11" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8.48633 10.4004C8.73047 10.4004 8.97461 10.3027 9.14062 10.1172L16.6992 2.37305C16.8652 2.20703 16.9629 1.99219 16.9629 1.74805C16.9629 1.24023 16.582 0.849609 16.0742 0.849609C15.8301 0.849609 15.6055 0.947266 15.4395 1.10352L7.95898 8.75H9.00391L1.52344 1.10352C1.36719 0.947266 1.14258 0.849609 0.888672 0.849609C0.380859 0.849609 0 1.24023 0 1.74805C0 1.99219 0.0976562 2.20703 0.263672 2.38281L7.82227 10.1172C8.00781 10.3027 8.23242 10.4004 8.48633 10.4004Z" />
-        </svg>
-      </a>
+      <cta>
+        <a class="cta">Book a call now</a>
+      </cta>
       {inlineImport({ src: initTextScramble, selfExecute: true })}
     </home>
   );
@@ -54,8 +50,8 @@ function initTextScramble() {
   async function scrambleText() {
     const text = el.innerText;
 
-    // Slower scramble effect (30 frames, 80ms each) ✨
-    for (let frame = 0; frame < 30; frame++) {
+    // Reduced frames (10 instead of 30) but same speed per frame ⚡
+    for (let frame = 0; frame < 10; frame++) {
       let output = "";
       for (let i = 0; i < text.length; i++) {
         if (text[i] === "\n") {
@@ -71,17 +67,17 @@ function initTextScramble() {
         }
       }
       el.innerText = output;
-      await new Promise(r => setTimeout(r, 80)); // Slower frame rate
+      await new Promise(r => setTimeout(r, 80)); // Keeping same frame rate
     }
 
     el.innerText = text;
   }
 
-  // Start animation loop with longer pauses 🔄
+  // Start animation loop 🔄
   async function animate() {
     while (true) {
       await scrambleText();
-      await new Promise(r => setTimeout(r, 8000)); // 8 second pause between animations
+      await new Promise(r => setTimeout(r, 8000));
     }
   }
 
